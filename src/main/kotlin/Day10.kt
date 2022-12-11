82ceba7b788613e2f1a98a4e725607ac9871117d
Osvaldas Bernatavičius <Osvaldas.Bernatavicius@4finance.com>
2022-12-11 21:58:22 +0200

    Day 10 solved.

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -4,16 +4,51 @@ fun main() {
     val day10 = Day10()
     val input = loadInput(10).filter { it.isNotEmpty() }
     println(day10.first(input))
-    println(day10.second(input))
+    day10.second(input)
 }
 
 class Day10 {
+    private var signalStrength = 0
+    private var cycle = 1
+
     fun first(input: List<String>): Int {
-        return 0
+        var xValue = 1
+        input.forEach { line ->
+            if (line == "noop") processCycle(xValue)
+            else {
+                processCycle(xValue)
+                processCycle(xValue)
+                xValue += line.substringAfter(" ").toInt()
+            }
+        }
+        return signalStrength
     }
 
-    fun second(input: List<String>): Int {
-        return 0
+    fun second(input: List<String>) {
+        var xValue = 1
+        signalStrength = 0
+        cycle = 0
+        input.forEach { line ->
+            if (line == "noop") processCycleAndPrint(xValue)
+            else {
+                processCycleAndPrint(xValue)
+                processCycleAndPrint(xValue)
+                xValue += line.substringAfter(" ").toInt()
+            }
+        }
+    }
+
+    private fun processCycle(x: Int) {
+        if (cycle % 40 == 20) signalStrength += cycle * x
+        cycle++
+    }
+
+    private fun processCycleAndPrint(x: Int) {
+        val positionInLine = cycle % 40
+        if (positionInLine == 0) println()
+        if (positionInLine in x - 1..x + 1) print("#")
+        else print(".")
+        cycle++
     }
 
 }
